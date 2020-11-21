0d6a7ff2fde77ebc10e86caa47aa2734c03159a5
Ariel <arielmello@rede.ulbra.br>
2020-11-21 09:57:13 -0300

    home e routes

--- www/codeigniter4/app/Config/App.php
+++ www/codeigniter4/app/Config/App.php
@@ -21,7 +21,8 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost:8080/';
+	//public $baseURL = 'http://localhost:8080/';
+	public $baseURL = 'http://localhost:8080/pw_ulbra/www/codeigniter4/public';
 
 	/*
 	|--------------------------------------------------------------------------
---
+++ www/codeigniter4/app/Controllers/Site.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\Controller;
+
+class Site extends Controller{
+
+    public function index(){
+        $this->view();
+    }
+
+    public function view($page = 'home'){
+
+        $data['title'] = ucfirst($page);
+
+        echo view('templates/header.php', $data);
+        echo view('site/'.$page . '.php', $data);
+        echo view('templates/footer.php', $data);
+    }
+}
